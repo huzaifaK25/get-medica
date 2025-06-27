f91bcb47b430c9bcd5798245251c1c820fd1dd24
Huzaifa Kashif <huzaifakashif202@gmail.com>
2025-06-27 22:02:59 +0500

    working on responsive design

--- src/app/(home-page)/layout.tsx
+++ src/app/(home-page)/layout.tsx
@@ -1,5 +1,5 @@
 import HomeLogo from '@/components/home.logo';
-import { FiBell, FiUser } from 'react-icons/fi';
+import { FiBell, FiBox, FiUser } from 'react-icons/fi';
 import SvgAppointment from '@/components/svg.appointment';
 import SvgAvailability from '@/components/svg.availability';
 import SvgDoctor from '@/components/svg.doctor';
@@ -7,18 +7,15 @@ import SvgDoctor from '@/components/svg.doctor';
 interface Props {
   children: React.ReactNode;
 }
+
 type Role = 'doctor' | 'patient';
-// enum Role {
-//   DOCTOR = 'doctor',
-//   PATIENT = 'patient'
-// }
 const role: Role = 'doctor';
 
 const HomeLayout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="flex flex-row h-screen w-vw overflow-x-hidden">
+    <div className="flex flex-row h-screen w-vw overflow-x-hidden ">
       {/* side nav bar */}
-      <nav className=" w-1/6 bg-[var(--primary-color)]">
+      <nav className=" hidden w-2xs bg-[var(--primary-color)] md:block ">
         <HomeLogo />
         <div className="flex flex-col mt-10 items-center gap-3">
           {role === 'doctor' ? (
@@ -47,15 +44,15 @@ const HomeLayout: React.FC<Props> = ({ children }) => {
       {/* right panel */}
       <div className=" w-full">
         {/* top nav bar */}
-        <div className="bg-gray-200  h-1/9 flex justify-end">
+        <div className="bg-gray-200  h-1/9 flex  justify-between md:justify-end">
           {/* profile box */}
-          <div className=" w-100 h-full flex flex-row justify-evenly items-center">
+          <div className="w-60 md:w-100 h-full flex flex-row justify-evenly items-center">
             <div>
-              <div className="bg-[var(--primary-color)] w-15 h-15 rounded-full flex items-center justify-center">
+              <div className="hidden bg-[var(--primary-color)] w-15 h-15 rounded-full md:flex items-center justify-center">
                 <FiBell className="text-white w-10 h-10" />
               </div>
             </div>
-            <div className=" w-40 flex flex-col justify-center items-start">
+            <div className=" w-30 md:w-40 text-[15px] lg:text-[20px] flex flex-col justify-center items-start">
               <p>Welcome</p>
               <p className="font-bold">Dr. John Doe</p>
             </div>
@@ -64,6 +61,9 @@ const HomeLayout: React.FC<Props> = ({ children }) => {
                 <FiUser className="text-white w-10 h-10" />
               </div>
             </div>
+          </div>
+          <div className="md:hidden text-3xl mt-auto mb-auto mr-10 items-center">
+            <FiBox />
           </div>
         </div>
         {/* content page */}
--- src/components/patient.home.view.tsx
+++ src/components/patient.home.view.tsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 const PatientHomeView = () => {
   return (
-    <div className="text-primary">
+    <div className="">
       <div>PATIENT HOME PAGE</div>
-      <div className="text-secondary">Hello world</div>
+      {/* <div className="border-2 border-amber-600 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+        <div className="p-5">item</div>
+      </div> */}
     </div>
   );
 };

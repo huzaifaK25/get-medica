856bca0af73973cfc3ce5553eab422ccbe89d39e
Huzaifa Kashif <huzaifakashif202@gmail.com>
2025-07-04 16:17:57 +0500

    state after checkbox input

--- src/app/(dashboard)/doctor-dashboard/page.tsx
+++ src/app/(dashboard)/doctor-dashboard/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React, { useState } from 'react';
-import TableRow from '../../../components/table.row';
 import { useGetUser } from '@/services/queries/doctor.query';
 import { useSetSchedule } from '@/services/mutations/set-schedule.mutation';
 
@@ -8,24 +7,51 @@ import { useSetSchedule } from '@/services/mutations/set-schedule.mutation';
 const DoctorHome = () => {
   // get logged in doctor profile
   const { data, error, status } = useGetUser();
-  console.log(data?.user?.id);
+  // console.log(data?.user?.id);
+  const [weekdays] = useState([
+    { id: 1, day: 'Monday', abbr: 'Mon' },
+    { id: 2, day: 'Tuesday', abbr: 'Tue' },
+    { id: 3, day: 'Wednesday', abbr: 'Wed' },
+    { id: 4, day: 'Thursday', abbr: 'Thurs' },
+    { id: 5, day: 'Friday', abbr: 'Fri' },
+  ]);
+  let [selectedDays, setSelectedDays] = useState<string[]>([]);
 
-  // set schedule
+  // handles checkbox toggle to add or remove days
+  const handleToggle = (checked: boolean, dayId: number) => {
+    const abbrv = weekdays.find((day) => day.id === dayId)?.abbr;
+    if (!abbrv) return;
+
+    if (checked) {
+      selectedDays = selectedDays.includes(abbrv)
+        ? selectedDays
+        : [...selectedDays, abbrv];
+      setSelectedDays(selectedDays);
+    } else if (!checked) {
+      selectedDays = selectedDays.filter((a) => a !== abbrv);
+      setSelectedDays(selectedDays);
+    }
+    console.log(selectedDays);
+    // console.log(dayId, checked, abbrv);
+  };
+
+  //TODO: function to handle timebox inputs:
+
+  // post schedule
   const { mutateAsync, isPending } = useSetSchedule();
-
   const setDoctorSchedule = async () => {
     // post
-    // await mutateAsync(
-    //   {
-    //     doctor_id: 0,
-    //     days: [],
-    //     timings: [],
-    //   },
-    //   {
-    //     onSuccess(data, variables, context) {},
-    //     onError(error, variables, context) {},
-    //   }
-    // );
+    await mutateAsync(
+      {
+        doctor_id: 0,
+        days: selectedDays,
+        timings: [],
+      },
+      {
+        onSuccess(data, variables, context) {},
+        onError(error, variables, context) {},
+      }
+    );
   };
 
   // TODO:
@@ -58,16 +84,46 @@ const DoctorHome = () => {
           <p className="w-[82px] md:w-[140px] text-gray-400">Sunday</p>
           <p className="grow-1  text-gray-400">Unavailable</p>
         </div>
-        {/* row - monday  */}
-        <TableRow day="Monday" />
-        {/* row - tuesday */}
-        <TableRow day="Tuesday" />
-        {/* row - wednesday */}
-        <TableRow day="Wednesday" />
-        {/* row - thursday */}
-        <TableRow day="Thursday" />
-        {/* row - friday */}
-        <TableRow day="Friday" />
+        {/* MONDAY_FRIDAY ROWS */}
+        {weekdays.map((day) => (
+          <div
+            key={day.id}
+            className="flex flex-row items-center gap-10 lg:gap-20 text-[15px]"
+          >
+            <div className="h-5 w-5">
+              <input
+                className="peer border-1 border-gray-500 h-5 w-5"
+                type="checkbox"
+                onChange={(e) => handleToggle(e.target.checked, day.id)}
+              />
+            </div>
+            <p className="w-[82px] md:w-[140px]  peer-checked:text-black text-gray-400">
+              {day.day}
+            </p>
+            {/* Time inputs */}
+            <div className="flex gap-2 items-center justify-center peer-checked:text-black text-gray-400">
+              <p className="mr-2 ">From:</p>
+              <input
+                className="border-1 border-gray-300 rounded-[5px] px-2 py-1 lg:w-50 "
+                type="time"
+              />
+              <p className="mr-2">To:</p>
+              <input
+                className="border-1 border-gray-300 rounded-[5px] px-2 py-1 lg:w-50 "
+                type="time"
+              />
+            </div>
+            <div className="flex gap-6 md:gap-10 lg:gap-15 xl:gap-20">
+              <button className="peer-checked:bg-primary bg-gray-400 text-white font-semibold text-xl h-5 w-5 rounded-[4px] flex items-center justify-center pb-1">
+                +
+              </button>
+              <button className="bg-white peer-checked:text-gray-500 text-gray-400 font-semibold text-xl h-5 w-5 border-1 border-gray-500 rounded-[4px] flex items-center justify-center pb-1">
+                -
+              </button>
+            </div>
+          </div>
+        ))}
+
         {/* row - saturday */}
         <div className="flex flex-row items-center gap-10 lg:gap-20 text-[15px]">
           <input
--- src/components/table.row.tsx
+++ src/components/table.row.tsx
@@ -5,7 +5,7 @@ interface Props {
   day: string;
 }
 
-const TableRow: React.FC<Props> = ({ day }) => {
+const TableRow: React.FC<Props> = () => {
   // const [checked, setChecked] = useState(false);
   const [days, setDays] = useState<string[]>([]);
   // console.log(checked);
@@ -15,41 +15,7 @@ const TableRow: React.FC<Props> = ({ day }) => {
     console.log(value);
   };
 
-  return (
-    <div className="flex flex-row items-center gap-10 lg:gap-20 text-[15px]">
-      <div className="h-5 w-5">
-        <input
-          id="day"
-          className="peer border-1 border-gray-500 h-5 w-5"
-          type="checkbox"
-          onChange={(e) => daysArray(e.target.checked)}
-        />
-      </div>
-      <p className="w-[82px] md:w-[140px]  peer-checked:text-black text-gray-400">
-        {day}
-      </p>
-      <div className="flex gap-2 items-center justify-center peer-checked:text-black text-gray-400">
-        <p className="mr-2 ">From:</p>
-        <input
-          className="border-1 border-gray-300 rounded-[5px] px-2 py-1 lg:w-50 "
-          type="time"
-        />
-        <p className="mr-2">To:</p>
-        <input
-          className="border-1 border-gray-300 rounded-[5px] px-2 py-1 lg:w-50 "
-          type="time"
-        />
-      </div>
-      <div className="flex gap-6 md:gap-10 lg:gap-15 xl:gap-20">
-        <button className="peer-checked:bg-primary bg-gray-400 text-white font-semibold text-xl h-5 w-5 rounded-[4px] flex items-center justify-center pb-1">
-          +
-        </button>
-        <button className="bg-white peer-checked:text-gray-500 text-gray-400 font-semibold text-xl h-5 w-5 border-1 border-gray-500 rounded-[4px] flex items-center justify-center pb-1">
-          -
-        </button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default TableRow;
